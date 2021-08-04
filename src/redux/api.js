d439940d49b9b5a864941599a085c06b7a4229ea
luchosr <luchosr@gmail.com>
2021-08-04 19:25:13 +0200

    update reducer

--- src/redux/api.js
+++ src/redux/api.js
@@ -4,6 +4,6 @@ const YOUR_APP_KEY = "5921ba20055f2db26a9830b8f3d3c385";
 const YOUR_APP_ID = "9d4e696c";
 
 export const getRecepies = async (query) => {
-  const url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
   await axios.get(url);
 };
---
+++ src/redux/reducer.js
@@ -0,0 +1,31 @@
+import * as types from "./actionTypes";
+
+const initialState = {
+  recipes: [],
+  error: null,
+  loading: false,
+};
+
+const recipeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.FETCH_RECIPE_START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case types.FETCH_RECIPE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        recipes: action.payload,
+      };
+    case types.FETCH_RECIPE_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+  }
+};
+
+export default recipeReducer;
