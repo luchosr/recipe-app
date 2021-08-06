6f64b40066f6873b1377731ba7ea11a07afab3fc
luchosr <luchosr@gmail.com>
2021-08-06 10:06:55 +0200

    add card component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { TextField, makeStyles, Button } from "@material-ui/core";
+import { TextField, makeStyles, Button, Grid } from "@material-ui/core";
 import * as types from "./redux/actionTypes";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import DirectionsRunIcon from "@material-ui/icons/DirectionsRun";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,10 +29,54 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const gridStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+const cardStyles = makeStyles((theme) => ({
+  root: {
+    width: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
 function App() {
   const classes = useStyles();
+  const gridClasses = gridStyles();
+  const cardClasses = cardStyles();
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("chicken");
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const { recipes } = useSelector((state) => state.data);
 
   const updateSearch = () => {
     setQuery(search);
@@ -47,6 +107,84 @@ function App() {
           Search
         </Button>
       </form>
+      <Grid container className={gridClasses.root} spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing={2}>
+            {recipes &&
+              recipes.hits &&
+              recipes.hits.map((item, index) => (
+                <Grid key={index} item>
+                  <Card className={cardClasses.root}>
+                    <CardHeader
+                      avatar={
+                        <Avatar
+                          aria-label="recipe"
+                          className={cardClasses.avatar}
+                        >
+                          R
+                        </Avatar>
+                      }
+                      action={
+                        <IconButton aria-label="settings">
+                          <MoreVertIcon />
+                        </IconButton>
+                      }
+                      title={item.recipe.label}
+                      subheader={
+                        <span>
+                          <DirectionsRunIcon />
+                          {item.recipe.calories}
+                        </span>
+                      }
+                    />
+                    <CardMedia
+                      className={cardClasses.media}
+                      image={item.recipe.image}
+                      title={item.recipe.calories}
+                    />
+                    <CardContent>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                      ></Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                      <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                      </IconButton>
+                      <IconButton aria-label="share">
+                        <ShareIcon />
+                      </IconButton>
+                      <IconButton
+                        className={clsx(cardClasses.expand, {
+                          [cardClasses.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                      >
+                        <ExpandMoreIcon />
+                      </IconButton>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                      <CardContent>
+                        <Typography paragraph>Method:</Typography>
+                        <Typography paragraph></Typography>
+                        <Typography paragraph></Typography>
+                        <Typography paragraph></Typography>
+                        <Typography></Typography>
+                      </CardContent>
+                    </Collapse>
+                  </Card>
+                </Grid>
+              ))}
+          </Grid>
+        </Grid>
+        {/* {recipes &&
+          recipes.hits &&
+          recipes.hits.map((item) => <h4>{item.recipe.label}</h4>)} */}
+      </Grid>
     </div>
   );
 }
